824050146aad8df21587fa89291146e47abd1ca9
guinohenostroza <guinohenostroza@gmail.com>
2022-02-06 21:43:48 -0500

    s
    s

---
+++ src/PartsUnlimitedWebsite/ViewModels/AlbumData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartsUnlimited.ViewModels
+{
+ // Comment
+    public class ProductData
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+    }
+}
